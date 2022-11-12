1726033eecab160178df9a0fce9a180080c7ef7d
Jpdvx01 <88458329+Jpdvx01@users.noreply.github.com>
2022-11-12 14:25:12 +0100

    Delete Loader.lua

--- Loader.lua
+++
@@ -1,6 +0,0 @@
----
---- Created by Jpdvx.
---- DateTime: 12/11/2022 0:40
----
----
-loadstring(game:HttpGet"https://raw.githubusercontent.com/Jpdvx01/Zeon-Hub/main/Zeon-Hub.lua?token=GHSAT0AAAAAAB2I4EUXFNNURR4UEMPSS6D4Y3O6DDQ")()
